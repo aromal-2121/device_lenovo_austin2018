35ea357a14ed6199d6ac5a4e6b9a9c683ce7eae8
aromal-2121 <aromalseee@gmail.com>
2025-06-27 13:00:27 +0530

    Update device.mk

--- device/lenovo/austin2018/device.mk
+++ device/lenovo/austin2018/device.mk
@@ -1,1 +1,9 @@
+# device.mk for Lenovo K9 Note (austin2018)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.device=austin2018 \
+    ro.build.product=austin2018
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab
 
