688ed8be4e0da509770b402cd47d107362529eae
aromal-2121 <aromalseee@gmail.com>
2025-06-27 13:01:06 +0530

    Update Android.mk

--- device/lenovo/austin2018/Android.mk
+++ device/lenovo/austin2018/Android.mk
@@ -1,2 +1,7 @@
-# Android.mk - placeholder
-include $(call all-subdir-makefiles)
+# Android.mk
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter austin2018,$(TARGET_DEVICE)),)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
